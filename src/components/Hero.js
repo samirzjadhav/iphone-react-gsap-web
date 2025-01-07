755f45aef069cea8239a8e2674aff52a6b9cf1c0
samir jadhav <samirzjadhav@gmail.com>
2025-01-07 09:22:21 +0530

    feat: make a state to change a video for a small scrren and desktop version and its look goood

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-  return (
-    <div>
-      Hero
-    </div>
-  )
-}
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
 
-export default Hero
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12 ">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
