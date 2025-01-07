d3953709721a42c2e84b7350008d553f96634cca
samir jadhav <samirzjadhav@gmail.com>
2025-01-07 09:55:06 +0530

    feat: complete a top of hightlight section

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { rightImg, watchImg } from "../utils";
 
 const Highlight = () => {
-  return (
-    <div>
-      Highlight
-    </div>
-  )
-}
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
 
-export default Highlight
+  return (
+    <section
+      id="hightlights"
+      className="w-scrren overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="scrren-max-width">
+        <div className="mb-12 w-full md:flex item-end justify-between">
+          <h1 id="title" className="section-heading">
+            Get the Highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlight;
