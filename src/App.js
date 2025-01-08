d7b4019f3f2b076caba64e9e6e918de1d3885875
samir jadhav <samirzjadhav@gmail.com>
2025-01-08 12:15:16 +0530

    feat: add a model.js file for three js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
+import Model from "./components/Model";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlight />
+      <Model />
     </main>
   );
 }
