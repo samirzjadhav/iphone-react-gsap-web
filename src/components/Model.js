3feb7729f160eeb92c94bbc24f58e315f017402f
samir jadhav <samirzjadhav@gmail.com>
2025-01-08 15:19:01 +0530

    feat: added model section on it

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,7 +1,48 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
+import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
 
 const Model = () => {
-  return <div>Model</div>;
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8f8a81", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  });
+
+  // Camera Control for the model view
+
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  useGSAP(() => {
+    gsap.to(
+      "#heading",
+      {
+        y: 0,
+        opacity: 1,
+      },
+      []
+    );
+  });
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look
+        </h1>
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Model;
