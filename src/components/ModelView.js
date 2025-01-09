200883d1597be15768af0e2d490497c5553a1c07
samir jadhav <samirzjadhav@gmail.com>
2025-01-09 14:00:02 +0530

    feat: enhance ModelView component with props and 3D view setup

--- src/components/ModelView.js
+++ src/components/ModelView.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { View, PerspectiveCamera } from "@react-three/drei";
 
-const ModelView = () => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
-    <div>ModelView</div>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-600 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera />
+    </View>
+  );
+};
 
-export default ModelView
+export default ModelView;
