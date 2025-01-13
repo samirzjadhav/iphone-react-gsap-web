668cf163feab9a7245fcd451f52649d1ee2c319e
samir jadhav <samirzjadhav@gmail.com>
2025-01-13 13:40:42 +0530

    feat: added dependency of sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ec1cef9536ba14913b306f06a5f57a5f@o4508635252195328.ingest.us.sentry.io/4508635253964800",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
