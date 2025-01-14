a6c99e79c8e41de2d01acad22e9a01faf6e840c3
samir jadhav <samirzjadhav@gmail.com>
2025-01-14 11:10:53 +0530

    feat: added new feature section on it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Highlight />
       <Model />
+      <Features />
     </main>
   );
 }
